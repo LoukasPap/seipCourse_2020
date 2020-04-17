e4de32e2c36261885a89daa6c4d54100c8ddfe2b
loukas pap <t8170101@aueb.gr>
2020-04-17 23:56:17 +0300

    Fix mistake at testing multiply function and add all its necessary test cases

--- seip2020_practical_assignments/unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -12,24 +12,57 @@ public class ArithmeticOperationsTest {
 	ArithmeticOperations ao = new ArithmeticOperations();
 	
 	@Test(expected = ArithmeticException.class)
-	public void divide_zeroDenominator() {
+	public void test_divide_zeroDenominator() {
 		ao.divide(2.5, 0.0);
 	}
 	
 	@Rule
-	public ExpectedException thrown = ExpectedException.none();
-	
-//IllegalArgumentException
-	@Test
-	public void multiply_negativeArgument_X(){
-		ao.multiply(-1, 3);
-	}
-	
-	
-	@Ignore
-	@Test
-	public void multiply_XequalsY() throws IllegalArgumentException{
-		ao.multiply(3, 3);
-	}
+	public ExpectedException thrown = ExpectedException.none();	
+		@Test
+		public void test_multiply_negativeArgument_X(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("x & y should be >= 0");
+			ao.multiply(-1, 3);
+		}
+		@Test
+		public void test_multiply_negativeArgument_Y(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("x & y should be >= 0");
+			ao.multiply(3, -1);
+		}		
+		@Test
+		public void test_multiply_XequalsY(){
+			ao.multiply(3, 3);
+		}
+		@Test
+		public void test_multiply_maxValue_X(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("The product does not fit in an Integer variable");
+			ao.multiply(2147483647, 3);
+		}
+		@Test
+		public void test_multiply_maxValue_Y(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("The product does not fit in an Integer variable");
+			ao.multiply(3, 2147483647);
+		}
+		@Test
+		public void test_multiply_minValue_X(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("x & y should be >= 0");
+			ao.multiply(-2147483647, 3);
+		}
+		@Test
+		public void test_multiply_minValue_Y(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("x & y should be >= 0");
+			ao.multiply(3, -2147483647);
+		}
+		@Test
+		public void test_multiply_maxValues_XandY(){
+			thrown.expect(IllegalArgumentException.class);
+			thrown.expectMessage("The product does not fit in an Integer variable");
+			ao.multiply(2147483647, 2147483646);
+		}
 		
 }
