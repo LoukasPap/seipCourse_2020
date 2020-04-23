41600ac785417f20721c6e890b44abe74306af28
loukas pap <t8170101@aueb.gr>
2020-04-23 03:34:18 +0300

    Create ArrayOperations

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
@@ -4,6 +4,7 @@
 package math;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import io.FileIO;
 
@@ -14,17 +15,24 @@ import io.FileIO;
 public class ArrayOperations {
 	
 	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath MyMath) {
-		int[] primeArray = {};
+		
+		List<Integer> primeArray = new ArrayList<>();
 		int[] fileArray = fileIo.readFile(filepath);
 		
-		for(int i=0;i < fileArray.length;i++) {
+		for(int i=0; i < fileArray.length; i++) {
 			if(MyMath.isPrime(fileArray[i])) {
-				primeArray[i] = fileArray[i];
+				primeArray.add(fileArray[i]);
 			}
 		}
-		return primeArray;
-	
-	}
+		
+		return primeArray.stream().mapToInt(i -> i).toArray();
 
+	}
+//	FileIO fileIo = new FileIO();
+//	MyMath MyMath = new MyMath();
+//	"C:\\Users\\louka\\seipCourse\\seipCourse_2020\\seip2020_practical_assignments\\unittesting\\src\\test\\resources\\findPrimes.txt"
+//	public static void main(String[] args) {
+//	findPrimesInFile();
+//}
 	
 }
