1b488a709620f138c5a272c638dcab5ba63b98c4
loukas pap <t8170101@aueb.gr>
2020-06-01 00:15:20 +0300

    Add test class for the exporters

--- seip2020_practical_assignments/sourcecodeanalyzer/src/test/java/codeanalyzer/MetricsExporterTest.java
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/test/java/codeanalyzer/MetricsExporterTest.java
@@ -5,19 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import codeanalyzer.MetricsExporter;
+import codeanalyzer.MetricsExporterFactory;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doCallRealMethod;
 
+/**
+ * A class that tests the exporters
+ * 
+ * @author loukas
+ * @since May 2020
+ */
 public class MetricsExporterTest {
-	
-	MetricsExporter mex = new MetricsExporter();
-	
+
+	/* 
+	 * A reference to the MetricsExporterFactory class 
+	 * which methods we are testing in this class
+	 */
+	MetricsExporterFactory mex = new MetricsExporterFactory();
+
+	/*
+	 * A test case for when the exporter input is csv.
+	 */
 	@Test
 	public void testWriteCsv() {
 		// create the metrics content
@@ -25,11 +39,11 @@ public class MetricsExporterTest {
 		metrics.put("loc",30);
 		metrics.put("nom",5);
 		metrics.put("noc",2);
-		
+
 		// generate and write the output file
 		String outputFilepath = "src/test/resources/output_metrics";
 		mex.writeFile("csv", metrics, outputFilepath);
-		
+	
 		// evaluate that the file exists
 		File outputFile = new File(outputFilepath + ".csv");
 		Assert.assertTrue(outputFile.exists());
@@ -38,9 +52,12 @@ public class MetricsExporterTest {
 		outputFile.delete();
 	}
 	
+	/*
+	 * A test case for when the exporter input is json
+	 */
 	@Test
 	public void testWriteJson() {
-		MetricsExporter mockedExporter = mock(MetricsExporter.class);
+		MetricsExporterFactory mockedExporter = mock(MetricsExporterFactory.class);
 		// create an empty metrics content
 		Map<String, Integer> metrics = new HashMap<>();
 		String outputFilepath = "whatever-path";
@@ -51,9 +68,19 @@ public class MetricsExporterTest {
 		//just verify that the method was executed/called
 		verify(mockedExporter).writeFile("json", metrics, outputFilepath);
 	}
+
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none(); //Initializing to .none()
 	
-	@Test(expected = IllegalArgumentException.class)
+	/*
+	 * A test case for the exception caused when the exporter input is invalid
+	 */
+	@Test
 	public void testWriteFileWithUknownFIleType() {
+		thrown.expect(IllegalArgumentException.class);	
+		thrown.expectMessage("The output file type is not valid.");
+
 		mex.writeFile("non-existing-type", null, null);
 	}
 
